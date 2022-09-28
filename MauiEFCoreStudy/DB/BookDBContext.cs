e14e66492f147fdfcd6df698bcce028066951550
nakamurakko <28667444+nakamurakko@users.noreply.github.com>
2022-09-28 15:29:13 +0900

    ログをコンソールに出力する。

--- MauiEFCoreStudy/DB/BookDBContext.cs
+++ MauiEFCoreStudy/DB/BookDBContext.cs
@@ -2,6 +2,7 @@
 using Microsoft.EntityFrameworkCore;
 using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -14,6 +15,10 @@ public class BookDBContext : DbContext
     {
         // https://docs.microsoft.com/ja-jp/ef/core/dbcontext-configuration/
         optionsBuilder.UseSqlServer("Data Source=(local);Initial Catalog=BookDB;User ID=BookUser;Password=bkusr;Persist Security Info=True");
+
+        // ログをコンソールに出力する。
+        // https://learn.microsoft.com/ja-jp/ef/core/logging-events-diagnostics/simple-logging#logging-to-the-console
+        optionsBuilder.LogTo(message => Debug.WriteLine(message));
     }
 
     public DbSet<Author> Authors { get; set; }
