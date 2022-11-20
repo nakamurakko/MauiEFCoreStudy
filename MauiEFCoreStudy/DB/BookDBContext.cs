898010ff9bfd92553b70ab1dff885e4ccb3bec1b
nakamurakko <28667444+nakamurakko@users.noreply.github.com>
2022-11-20 17:05:28 +0900

    Entity Framework Core 7.0 移行に伴い、破壊的変更の軽減策を追加。
    https://learn.microsoft.com/ja-jp/ef/core/what-is-new/ef-core-7.0/breaking-changes#encrypt-defaults-to-true-for-sql-server-connections

--- MauiEFCoreStudy/DB/BookDBContext.cs
+++ MauiEFCoreStudy/DB/BookDBContext.cs
@@ -14,7 +14,8 @@ public class BookDBContext : DbContext
     protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
     {
         // https://docs.microsoft.com/ja-jp/ef/core/dbcontext-configuration/
-        optionsBuilder.UseSqlServer("Data Source=(local);Initial Catalog=BookDB;User ID=BookUser;Password=bkusr;Persist Security Info=True");
+        // https://learn.microsoft.com/ja-jp/ef/core/what-is-new/ef-core-7.0/breaking-changes#encrypt-defaults-to-true-for-sql-server-connections
+        optionsBuilder.UseSqlServer("Data Source=(local);Initial Catalog=BookDB;User ID=BookUser;Password=bkusr;Persist Security Info=True;TrustServerCertificate=True");
 
         // ログをコンソールに出力する。
         // https://learn.microsoft.com/ja-jp/ef/core/logging-events-diagnostics/simple-logging#logging-to-the-console
