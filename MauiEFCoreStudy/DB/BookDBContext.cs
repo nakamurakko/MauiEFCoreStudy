4b644d5f0728d6cd5744c4bcaafff8c928293ad5
nakamurakko <28667444+nakamurakko@users.noreply.github.com>
2024-10-14 16:35:43 +0900

    参照 DB を SQLite に変更。

--- MauiEFCoreStudy/DB/BookDBContext.cs
+++ MauiEFCoreStudy/DB/BookDBContext.cs
@@ -10,7 +10,7 @@ public sealed class BookDBContext : DbContext
     {
         // https://docs.microsoft.com/ja-jp/ef/core/dbcontext-configuration/
         // https://learn.microsoft.com/ja-jp/ef/core/what-is-new/ef-core-7.0/breaking-changes#encrypt-defaults-to-true-for-sql-server-connections
-        optionsBuilder.UseSqlServer("Data Source=(local);Initial Catalog=BookDB;User ID=BookUser;Password=bkusr;Persist Security Info=True;TrustServerCertificate=True");
+        optionsBuilder.UseSqlite("database.sqlite");
 
         // ログをコンソールに出力する。
         // https://learn.microsoft.com/ja-jp/ef/core/logging-events-diagnostics/simple-logging#logging-to-the-console
