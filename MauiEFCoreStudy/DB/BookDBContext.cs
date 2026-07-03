749d8d2ef96fae5cf1de14b3e2ca46a0f27bb6ab
nakamurakko <28667444+nakamurakko@users.noreply.github.com>
2026-07-04 08:05:01 +0900

    作成日時、更新日時の設定をイベントで行う。

--- MauiEFCoreStudy/DB/BookDBContext.cs
+++ MauiEFCoreStudy/DB/BookDBContext.cs
@@ -1,11 +1,21 @@
 ﻿using MauiEFCoreStudy.DB.Entities;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.ChangeTracking;
 using System.Diagnostics;
 
 namespace MauiEFCoreStudy.DB;
 
 public sealed class BookDbContext : DbContext
 {
+
+    /// <summary>
+    /// コンストラクター。
+    /// </summary>
+    public BookDbContext()
+    {
+        this.ChangeTracker.StateChanged += this.TimestampsChanged;
+        this.ChangeTracker.Tracked += this.TimestampsChanged;
+    }
 
     protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
     {
@@ -16,6 +26,32 @@ public sealed class BookDbContext : DbContext
         // ログをコンソールに出力する。
         // https://learn.microsoft.com/ja-jp/ef/core/logging-events-diagnostics/simple-logging#logging-to-the-console
         optionsBuilder.LogTo(message => Debug.WriteLine(message));
+    }
+
+    /// <summary>
+    /// 日時更新のイベント。
+    /// </summary>
+    /// <param name="sender">通知元のオブジェクト。</param>
+    /// <param name="e">イベントデータ。</param>
+    /// <remarks>
+    /// https://learn.microsoft.com/ja-jp/ef/core/logging-events-diagnostics/events
+    /// </remarks>
+    private void TimestampsChanged(object? sender, EntityEntryEventArgs e)
+    {
+        if (e.Entry.Entity is IHasDbTimestamps entityWithTimestamps)
+        {
+            switch (e.Entry.State)
+            {
+                case EntityState.Added:
+                    DateTime now = DateTime.Now;
+                    entityWithTimestamps.CreatedAt = now;
+                    entityWithTimestamps.UpdatedAt = now;
+                    break;
+                case EntityState.Modified:
+                    entityWithTimestamps.UpdatedAt = DateTime.Now;
+                    break;
+            }
+        }
     }
 
     public DbSet<Author> Authors { get; set; }
